3ea5fd8ce2e89111ce55ed08a8173b1c2a2479b7
Lucas Paguetti Pereira <lucaspaguetti2@gmail.com>
2025-12-15 19:13:13 -0300

    delete antigo alerta_reinicio.js

--- static/alerta_reinicio.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {const params = 
-    new URLSearchParams(window.location.search);
-    if (params.get("reset") === "true") {
-        alert("Sistema reiniciado!✅ Clique em OK para iniciar uma nova compra.🛍️🛒");
-    }});
-
-
